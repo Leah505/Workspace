7d2a309b8e1e1aca8436cd99a16efbb5bda01edf
Leah505 <leah.clarke@ada.ac.uk>
2024-10-21 09:59:19 +0000

    Variables and arrays

---
+++ example.js
@@ -0,0 +1,14 @@
+const readline = require("readline-sync")
+
+console.log("Hello World");
+
+myArr = [
+    56,21,32,7,64
+]
+
+console.log(myArr)
+console.log(myArr[2])
+
+for(let i = 0; i < 5; i++){
+    console.log(myArr[i])
+}
